18100462c0e2bfdd72b72b3c3815c432056b74a4
1ndiedevelper-lab <1ndiedevelper@gmail.com>
2025-08-20 07:59:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,40 +4,30 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
 
-
-// Serwujemy pliki statyczne z katalogu "public"
 app.use(express.static('public'));
 
-let users = [];  // lista nicków aktualnie zalogowanych
+let users = [];
 
 io.on('connection', (socket) => {
   let userNick = '';
 
-  // Użytkownik dołącza do czatu
   socket.on('join', (nick) => {
+    if (!nick || users.includes(nick)) {
+      socket.emit('nickname error', 'Nick jest zajęty lub nieprawidłowy');
+      return;
+    }
     userNick = nick;
     users.push(nick);
     io.emit('user list', users);
     console.log(`${nick} dołączył do czatu`);
   });
 
-  // Odbieramy i rozsyłamy wiadomość
   socket.on('chat message', (msg) => {
+    if (!msg || !msg.user || !msg.text) return;
     io.emit('chat message', msg);
     console.log(`Wiadomość od ${msg.user}: ${msg.text}`);
   });
 
-  // Użytkownik zaczął pisać
-  socket.on('typing', (nick) => {
-    socket.broadcast.emit('typing', nick);
-  });
-
-  // Użytkownik przestał pisać
-  socket.on('stop typing', (nick) => {
-    socket.broadcast.emit('stop typing', nick);
-  });
-
-  // Użytkownik rozłącza się
   socket.on('disconnect', () => {
     if (userNick) {
       users = users.filter(u => u !== userNick);
@@ -47,7 +37,6 @@ io.on('connection', (socket) => {
   });
 });
 
-// Start serwera
 http.listen(PORT, () => {
   console.log(`✅ Serwer działa na http://localhost:${PORT}`);
 });
